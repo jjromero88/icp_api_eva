140fa74fbc8cfdab4bbf6d9e704405f63468a219
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-07-10 21:27:14 -0500

    Feat: Implement Transversal.Logging

---
+++ PCM.SIP.ICP.EVA.Transversal.Logging/LoggerAdapter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using PCM.SIP.ICP.EVA.Transversal.Common;
+
+namespace PCM.SIP.ICP.EVA.Transversal.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+        }
+    }
+}
