3a5ccbd68114366434624c2d2db295a151f44a95
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-07-10 21:31:53 -0500

    Feat: Implement Transversal.Mapper project

---
+++ PCM.SIP.ICP.EVA.Transversal.Mapper/MappingsProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace PCM.SIP.ICP.EVA.Transversal.Mapper
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+
+        }
+    }
+}
