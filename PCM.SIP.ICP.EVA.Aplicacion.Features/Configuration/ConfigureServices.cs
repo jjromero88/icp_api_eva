fe9ca8925343828ceab07494059052cbed894323
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-07-10 21:58:41 -0500

    Feat: Implement Aplicacion.Features Project

---
+++ PCM.SIP.ICP.EVA.Aplicacion.Features/Configuration/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PCM.SIP.ICP.EVA.Aplicacion.Features
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
