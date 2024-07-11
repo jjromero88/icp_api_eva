a8eba196ae9f9d3e4d1479fc9e73d1462540eb9c
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-07-10 21:42:07 -0500

    Feat. implement Domain project and update class contracts

---
+++ PCM.SIP.ICP.EVA.Transversal.Contracts/Seguridad/Entities/UsuarioCache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCM.SIP.ICP.EVA.Transversal.Contracts.Seguridad.Entities
+{
+    public class UsuarioCache
+    {
+        public string? authkey { get; set; }
+        public string? usuariokey { get; set; }
+        public string? personakey { get; set; }
+        public string? perfilkey { get; set; }
+        public string? username { get; set; }
+        public string? perfil { get; set; }
+        public string? numdocumento { get; set; }
+        public string? nombre_completo { get; set; }
+    }
+}
---
+++ PCM.SIP.ICP.EVA.Transversal.Contracts/Seguridad/Response/ResponseSecurityService.cs
@@ -0,0 +1,20 @@
+﻿using PCM.SIP.ICP.EVA.Transversal.Contracts.Seguridad.Entities;
+using System.Text.Json.Serialization;
+
+namespace PCM.SIP.ICP.EVA.Transversal.Contracts.Seguridad.Response
+{
+    public class ResponseSecurityService
+    {
+        [JsonPropertyName("code")]
+        public int Code { get; set; }
+
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+
+        [JsonPropertyName("error")]
+        public bool Error { get; set; }
+
+        [JsonPropertyName("payload")]
+        public UsuarioCache? Payload { get; set; }
+    }
+}
