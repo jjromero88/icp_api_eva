a60dcffa98d5ca07c54e6561eb3650116e75a7f9
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-07-10 21:25:57 -0500

    Feat: implement contracts project

---
+++ PCM.SIP.ICP.EVA.Transversal.Contracts/Seguridad/Request/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCM.SIP.ICP.EVA.Transversal.Contracts.Seguridad.Request
+{
+    public class LoginRequest
+    {
+    }
+}
---
+++ PCM.SIP.ICP.EVA.Transversal.Contracts/Seguridad/Response/SeguridadResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCM.SIP.ICP.EVA.Transversal.Contracts.Seguridad.Response
+{
+    public class SeguridadResponse
+    {
+        public int code { get; set; }
+        public string? message { get; set; }
+        public bool error { get; set; }
+        public object? payload { get; set; }
+    }
+}
