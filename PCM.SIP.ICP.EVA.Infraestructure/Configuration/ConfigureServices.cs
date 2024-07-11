909db62b5445fe51dae98150ace62100cf5dfb4f
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-07-10 21:48:55 -0500

    Feat: Implement Infraestructure project

---
+++ PCM.SIP.ICP.EVA.Infraestructure/Configuration/ConfigureServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PCM.SIP.ICP.EVA.Infraestructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfraestructureServices(this IServiceCollection services)
+        {
+    
+            return services;
+        }
+    }
+}
---
+++ PCM.SIP.ICP.EVA.Infraestructure/Services/Account/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCM.SIP.ICP.EVA.Infraestructure.Services.Account
+{
+    internal class Class1
+    {
+    }
+}
