b166e6971a98914bd38ac5adfcbc82ccf49592c5
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-07-10 21:53:11 -0500

    Feat: Implement Persistence Project

---
+++ PCM.SIP.ICP.EVA.Persistence/Configuration/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PCM.SIP.ICP.EVA.Persistence.Context;
+
+namespace PCM.SIP.ICP.EVA.Persistence
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<DapperContext>();
+
+            return services;
+        }
+    }
+}
---
+++ PCM.SIP.ICP.EVA.Persistence/Repository/Base/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace PCM.SIP.ICP.EVA.Persistence.Repository.Base
+{
+    public class UnitOfWork
+    {
+        public UnitOfWork() { }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
