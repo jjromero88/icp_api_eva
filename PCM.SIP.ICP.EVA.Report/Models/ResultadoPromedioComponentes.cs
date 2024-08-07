14de6cd36d0bdcd1b5e386fa7a5c5dbb2b2fb1e5
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-08-07 00:05:49 -0500

    Feat: implement design ReporteResultadoComponentePorEtapaPorGrupo

---
+++ PCM.SIP.ICP.EVA.Report/Models/ResultadoPromedioComponentes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCM.SIP.ICP.EVA.Report
+{
+    public class ResultadoPromedioComponentes
+    {
+        public string categoria { get; set; }
+        public decimal resultado { get; set; }
+    }
+}
