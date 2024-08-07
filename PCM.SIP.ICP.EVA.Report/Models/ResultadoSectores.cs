86a94b4d689e74b25cd865c822878fff556164cc
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-08-07 01:40:10 -0500

    Feat: Design Reporte Componentes por Etapa

---
+++ PCM.SIP.ICP.EVA.Report/Models/ResultadoSectores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCM.SIP.ICP.EVA.Report
+{
+    public class ResultadoSectores
+    {
+        public string sector { get; set; }
+        public decimal resultado { get; set; }
+    }
+}
